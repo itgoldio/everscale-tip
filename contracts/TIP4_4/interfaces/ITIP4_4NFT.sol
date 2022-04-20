51abb2d0a7268016e1fe905f5c7c6f08fc960e05
nikitaghost <nekhd@yandex.ru>
2022-04-20 13:27:24 +0000

    Remove TIP4_4

--- contracts/TIP4_4/interfaces/ITIP4_4NFT.sol
+++
@@ -1,6 +0,0 @@
-pragma ton-solidity >= 0.58.0;
-
-interface ITIP4_4NFT {
-    function onStorageFillComplete(address gasReceiver) external;
-    function getStorage() external view responsible returns (address addr);
-}
